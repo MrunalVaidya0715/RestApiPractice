94e966659850ab3388e5ee9b942aed1be6259501
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2022-12-31 12:43:37 +0530

    Post req added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,27 @@
 import express from "express";
 const router = express.Router()
 
-router.get('/',(req, res) => res.send("UsersRoute"))
+const users = [
+    {
+        firstName : "John",
+        lastname : "Doe",
+        age: 25
+    },
+    {
+        firstName : "Jane",
+        lastname : "Doe",
+        age: 23
+    }
+]
+
+router.post('/', (req,res)=>{
+    const user = req.body
+    users.push(user)
+    console.log("Post Request Fired")
+    res.send(`Data Added for ${user.firstName} Successfully`)
+})
+
+
+router.get('/',(req, res) => res.send(users))
 
 export default router;
