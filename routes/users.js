d23d9a33d794194fbc93bf34d203ca4d780f54d0
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2022-12-31 13:04:22 +0530

    Params

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,25 @@
 import express from "express";
+import {v4 as uuidv4} from 'uuid'
+
 const router = express.Router()
 
-const users = [
-    {
-        firstName : "John",
-        lastname : "Doe",
-        age: 25
-    },
-    {
-        firstName : "Jane",
-        lastname : "Doe",
-        age: 23
-    }
-]
+const users = []
+
+
 
 router.post('/', (req,res)=>{
     const user = req.body
-    users.push(user)
+    users.push({...user, id: uuidv4()})
     console.log("Post Request Fired")
     res.send(`Data Added for ${user.firstName} Successfully`)
 })
 
+
+router.get('/:id',(req,res)=>{
+    const {id} = req.params
+    const foundedUser = users.find((user)=> user.id=== id)
+    res.send(foundedUser)
+})
 
 router.get('/',(req, res) => res.send(users))
 
