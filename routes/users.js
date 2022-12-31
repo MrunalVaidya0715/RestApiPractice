00c5147fde271e2b7544dae64806018becf43729
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2022-12-31 13:46:56 +0530

    Delete Req

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import {v4 as uuidv4} from 'uuid'
 
 const router = express.Router()
 
-const users = []
+let users = []
 
 
 
@@ -19,6 +19,13 @@ router.get('/:id',(req,res)=>{
     const {id} = req.params
     const foundedUser = users.find((user)=> user.id=== id)
     res.send(foundedUser)
+})
+
+
+router.delete('/:id', (req, res)=>{
+    const {id} = req.params
+    users = users.filter((user)=> user.id !== id)
+    res.send(`User with ${id} deleted Successfully`)
 })
 
 router.get('/',(req, res) => res.send(users))
