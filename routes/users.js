d204ffd2a9ea23e2ed9fdb7241a9096e856bb86e
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2022-12-31 14:03:44 +0530

    Patch req

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {v4 as uuidv4} from 'uuid'
 
+
 const router = express.Router()
 
 let users = []
@@ -26,6 +27,18 @@ router.delete('/:id', (req, res)=>{
     const {id} = req.params
     users = users.filter((user)=> user.id !== id)
     res.send(`User with ${id} deleted Successfully`)
+})
+
+router.patch('/:id', (req, res)=>{
+    const { id} = req.params
+    const {firstName, lastName,  age} = req.body
+
+    const user = users.find((user)=> user.id === id)
+    if (firstName) user.firstName = firstName
+    if (lastName) user.lastName =lastName
+    if (age) user.age = age
+    
+    res.send(`User with ${id} updated successfully`)
 })
 
 router.get('/',(req, res) => res.send(users))
